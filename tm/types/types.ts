43746af1f4769de03be373956385333af5d4dd8a
F1r3byte <f1r3byte@yandex.com>
2019-02-26 22:55:44 +0100

    added some typescript practice

---
+++ tm/types/types.ts
@@ -0,0 +1,31 @@
+let myString: string;
+let myNumber: number;
+let myBoolean: boolean;
+let myVariable: any;
+
+let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[];
+let varArr: any[];
+
+let strArrOther: Array<string>;
+let numArrOther: Array<number>;
+let boolArrOther: Array<boolean>;
+let anyArrOther: Array<any>;
+
+let strNumTuple: [string, number];
+
+myString = "Hello".slice(0, 3);
+myNumber = 1;
+myBoolean = false;
+myVariable = true;
+
+strArrOther = ["Hello", "World"];
+numArr = [1, 2, 3];
+boolArr = [true, false, true];
+
+strNumTuple = ['Hello', 4];
+
+let myVoid: void = undefined;
+let myNull: null = null;
+let myUndefined: undefined = null;
